0cdf7f27b2336caba985dd4c590b17477ae266ce
Berzok <34751752+Berzok@users.noreply.github.com>
2018-06-04 14:43:23 +0200

    Add files via upload

--- src/vue/FenetreAgenda.java
+++ src/vue/FenetreAgenda.java
@@ -6,9 +6,21 @@ import java.awt.GridLayout;
 import java.text.ParseException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 
 @SuppressWarnings("serial")
 public class FenetreAgenda extends JFrame {
+	
+	public JMenuBar menuBar = new JMenuBar();
+	public JMenu menuTimeline = new JMenu("Timelines");
+	public JMenu menuAdd = new JMenu("Ajouts");
+	public JMenuItem addTimeline = new JMenuItem("Ajouter une Timeline");
+	public JMenuItem addEvent = new JMenuItem("Ajouter un event à une Timeline");
+	public JMenuItem timelineTest1 = new JMenuItem("Timeline Test1");
+	public JMenuItem timelineTest2 = new JMenuItem("Timeline Test2");
 
 	/**
 	 * Launch the application.
@@ -37,8 +49,19 @@ public class FenetreAgenda extends JFrame {
 		setSize(750, 400);
 		this.setLayout(new GridLayout(2, 0));
 		PanelEvenement panelNord = new PanelEvenement();
-		PanelTimeline panelFrise = new PanelTimeline();
+		PanelTimeline panelFrise = new PanelTimeline();		
 		this.add(panelNord, BorderLayout.NORTH);
 		this.add(panelFrise, BorderLayout.SOUTH);
+		
+ 		this.add(panelFrise, BorderLayout.SOUTH);
+ 		this.menuTimeline.add(timelineTest1);
+		this.menuTimeline.add(timelineTest2);
+		this.menuAdd.add(addTimeline);
+		this.menuAdd.add(addEvent);
+		this.menuBar.add(menuTimeline);
+		this.menuBar.add(menuAdd);
+		this.setJMenuBar(menuBar);
+		this.setVisible(true);
+		
 	}
 }
